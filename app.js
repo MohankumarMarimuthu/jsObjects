66e7265756c028182738f4e39c0f705b013e09fe
Mohankumar Marimuthu <mk1325612@gmail.com>
2023-11-30 08:51:09 +0530

    added few more object methods
    added object assign, create, seal , freeze, keys, values

--- app.js
+++ app.js
@@ -284,3 +284,93 @@ console.log("ArrayBuffer" ,z)
 const y = []
 y[0] = "1";
 console.log("y" , y)
+
+const target = {name: "mohan"};
+const source1 = {name: "mohankumar" , age: 26 , single : true};
+const source3 = {language: "flutter" , age: NaN , single : false , vip: true};
+console.log(Object.assign(target, source1 , source3))
+
+const job = {
+    position: "cashier",
+    type: "hourly",
+    isAvailable: true,
+    showDetails(){
+        const accepting = this.isAvailable ? 'is accepting applications' : "is not currently accepting applications";
+        
+        console.log(`The ${this.position} position is ${this.type} and ${accepting}.`);
+    }
+}
+// Object.create() we can use entire object key-values, methods on other object
+const barista = Object.create(job)
+barista.position = "barista"
+barista.type = "weekly"
+barista.showDetails()
+
+// this is function borrowing used to borrow function from other object
+const dev = {
+    position: "software developer",
+    type: "monthly",
+    isAvailable: false,
+}
+
+const dev1 = {
+    position: "customer service associate",
+    type: "monthly",
+    isAvailable: "i don't want to go there",
+}
+
+job.showDetails.call(dev)
+
+Object.keys(dev).forEach((item) => {
+    console.log("the key is: ",  item ,"value is" , dev[item] )
+})
+
+Object.values(dev1).map((item) => {
+    console.log(item)
+})
+
+
+// it is nothing do with the content but it is useful
+const formData = new FormData();
+formData.append("firstName" , "mohankumar")
+formData.append("lastName" , "marimuthu")
+formData.forEach((key , value) => {
+    // console.log("key", value ,key)
+})
+
+const entries = Object.entries(dev)
+
+
+// freezing object won't throw any error it will completely freeze the object we can't
+// add any new property or alter any property if we assign it to other value also we can't do anything
+const user = {
+    name: "mohan",
+    password: "me",
+}
+
+const freeze = Object.freeze(user)
+
+user.bro = "gowtham"
+console.log(user)
+
+// seal won't throw any error it will seal the object we can't add any new property
+// but we can alter existing properties , we can directly modify or assign it to other variable and modify
+
+const login = {
+    username: "mohankumar",
+    password: "123"
+}
+
+const seal = Object.seal(login)
+
+login.username = "mohankumar Marimuthu"
+seal.username = "gowtham"
+login.movie = "vikram"
+
+console.log(login)
+
+// getPrototype
+let nums = [1,2,3,4]
+const numsProto = Object.getPrototypeOf(nums) // we can pass array value and get prototype of that value which is equal to Array.prototype
+const loginProto = Object.getPrototypeOf(login) // we can pass object value and get prototype of that value which is equal to Object.prototype
+console.log(numsProto , loginProto)
